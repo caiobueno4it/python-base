6cbdccb2001679c569d475ab0a99426c00db0159
Caio Bueno <cloudadmin@VMDEVOPS001.zuoe51ddxziu3cyw2omfee0l0c.cx.internal.cloudapp.net>
2023-10-07 15:43:34 +0000

    Adicionado SHEBANG no script

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Exibir texto
 print('Hello Python!')
+
+# Exibir texto em letras minusculas
 print('Hello Python!'.lower())
+
+# Exibir texto em letras maisculas
 print('Hello Python!'.upper())
