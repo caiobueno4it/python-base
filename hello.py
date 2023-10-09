fc43b5d805d1df42c3590c148cb91fb125247b8d
Caio Bueno <cloudadmin@VMDEVOPS001.zuoe51ddxziu3cyw2omfee0l0c.cx.internal.cloudapp.net>
2023-10-09 15:00:40 +0000

    Aicionado estrutura de condicional

--- hello.py
+++ hello.py
@@ -1,10 +1,51 @@
 #!/usr/bin/env python3
+"""Hello World Mult Linguas
 
-# Exibir texto
-print('Hello Python!')
+Dependendo da lingua configurada no ambiente 
+o programa exibe a mensagem correspondente
 
-# Exibir texto em letras minusculas
-print('Hello Python!'.lower())
+Como usar:
+Necessario utilizar as informações carregadas na variável LANG 
 
-# Exibir texto em letras maisculas
-print('Hello Python!'.upper())
+Exemplo: 
+    export LAN=pt_BR
+
+Executar:
+    python3 hello.py
+        ou
+    ./hello.py
+"""
+
+__version__ = "0.0.1"
+__author__ = "Caio Bueno"
+__license__ = "Unlicense"
+
+import os
+language = os.getenv("LANG", "en_US")[:5]
+# export LANG=en_US.utf8
+# export LANG=pt_BR.utf8
+
+msg_en = "Hello, Python!"
+msg_pt = "Olá, Python!"
+
+
+if language == "en_US":
+    # Exibir texto
+    print(msg_en)
+
+    # Exibir texto em letras minusculas
+    print(msg_en.lower())
+
+    # Exibir texto em letras maisculas
+    print(msg_en.upper())
+
+elif language == "pt_BR":
+    # Exibir texto
+    print(msg_pt)
+
+    # Exibir texto em letras minusculas
+    print(msg_pt.lower())
+
+    # Exibir texto em letras maisculas
+    print(msg_pt.upper())
+    
